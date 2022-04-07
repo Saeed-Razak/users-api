0e91bca856b3412c7af28bd63990d1249d11adf9
Saeed-Razak <saeedabdulrazak95@gmail>
2022-04-07 14:58:09 +0000

    first commit

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const users=[
+    {id:1, name:"John",age: 25},
+    {id:2, name:"Yaw",age: 35},
+    {id:3, name:"Badu",age: 15},
+    {id:4, name:"Ama",age: 17},
+    {id:5, name:"Kofi",age: 30},
+]
+module.exports=users
