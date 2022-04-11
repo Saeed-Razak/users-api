84526190b945847c3360b8d21d83b07f025528c8
Saeed-Razak <saeedabdulrazak95@gmail>
2022-04-11 11:57:15 +0000

    added commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const userRoute = require("./routes/usersRoutes")
+const path = require("path")
 
 const app = express()
 
@@ -11,12 +12,16 @@ app.use(userRoute)
 
 //home route
 app.get('/', (req, res)=>{
-    res.status(200).send("<h1>WELCOME TO THE USERS DATABASE</h1>");
+    res.sendFile(path.join(__dirname + "/pages/index.html"))
+})
+
+app.get("/*", (req, res)=>{
+    res.status(400).sendFile(path.join(__dirname + "/pages/404.html"))
 })
 
 
 const PORT = 4000;
 
 app.listen(PORT, ()=>{
-    console.log("SERVER IS UP")
+    console.log("SERVER IS ACTIVE")
 })
